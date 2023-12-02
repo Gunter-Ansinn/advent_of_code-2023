098710520d1ad073cfcc20b3b3cb6302695f1d61
dragoncommands <35853220+dragonflame86@users.noreply.github.com>
2023-12-01 20:44:50 -0500

    Day 1 part 1 - almost Added: - Code to split up the file - Code to remove all non-numbers - Code to attempt to print the result

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,6 @@
 use std::fs::File;
 use std::io;
-use std::io::Read;
+use std::io::{Read, Write};
 
 pub fn read_file_as_str(file: &str) -> String {
     read_file_contents(file).unwrap()
@@ -11,3 +11,9 @@ pub fn read_file_contents(file: &str) -> io::Result<String> {
     file_data.read_to_string(&mut text)?;
     Ok(text)
 }
+
+
+// pub fn write_to_file(name : &str, content : &str) {
+//     let mut file = File::create(name)?;
+//     file.write_all(content.encode_utf16().)?;
+// }
